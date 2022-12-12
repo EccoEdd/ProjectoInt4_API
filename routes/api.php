221a292b443fb02ffcd35023aac3f1f8edce7dbd
EccoEdd <ceduardoemiliano@gmail.com>
2022-12-12 13:05:39 -0600

    New Route

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/hola', function (){
     \Illuminate\Support\Facades\Log::info("Hola Mundo");
+    dd("Hola");
     return "Hola mundo";
 });
 
