f1234f610f73b7d8a3b0b3f92f48b289c8a42817
EccoEdd <ceduardoemiliano@gmail.com>
2022-12-11 23:36:50 -0600

    Dioxide Ready

---
+++ app/Http/Controllers/DioxideController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Incubator;
+use App\Models\Ownership;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Dioxide;
+use Illuminate\Support\Facades\Validator;
+use PhpParser\Node\Stmt\Do_;
+
+class DioxideController extends Controller
+{
+    public function lastDioxideData(Request $request){
+        $validate = Validator::make($request->all(),[
+            'code' => 'required|size:5|exists:incubators',
+        ],[
+            'code' => [
+                'required' => 'You need the code of your incubator',
+                'size'     => 'This code is only 5 characters long',
+                'exists'   => 'The incubator must exists'
+            ]
+        ]);
+        if ($validate->fails())
+            return response()->json(['Message' => $validate->errors()], 403);
+
+        $incubator = Incubator::query()->where('code', '=', $request->code)->first();
+
+        $ownership = Ownership::query()
+            ->where('user_id', '=', $request->user()->id)
+            ->where('incubator_id', '=', $incubator->id)
+            ->first();
+
+        if(!$ownership)
+            return response()->json(["Message" => 'Unauthorized']);
+
+        $response = Http::withHeaders(['X-AIO-Key' => env('TOKEN_ADAJ')])
+            ->get('https://io.adafruit.com/api/v2/'.env('USER_ADAJ').'/feeds/humo/data?limit=1');
+
+        $dioxideOld = Dioxide::query()
+            ->where('identifier', '=', $response[0]['id'])
+            ->where('incubator_id', '=', $incubator->id)
+            ->first();
+
+        if($dioxideOld){
+            $dioxide = Dioxide::latest()->where('incubator_id', '=', $incubator->id)->first();
+            return response()->json(["Data" => $dioxide]);
+        }
+
+        $dioxide = new Dioxide();
+        $dioxide->value = $response[0]['value'];
+        $dioxide->identifier = $response[0]['id'];
+        $dioxide->incubator_id = $incubator->id;
+        $dioxide->save();
+
+        return response()->json(["Data" => $dioxide]);
+    }
+
+    public function dioxideData(Request $request){
+        $validate = Validator::make($request->all(),[
+            'code' => 'required|size:5|exists:incubators',
+        ],[
+            'code' => [
+                'required' => 'You need the code of your incubator',
+                'size'     => 'This code is only 5 characters long',
+                'exists'   => 'The incubator must exists'
+            ]
+        ]);
+        if ($validate->fails())
+            return response()->json(['Message' => $validate->errors()], 403);
+        $incubator = Incubator::query()->where('code', '=', $request->code)->first();
+
+        $ownership = Ownership::query()
+            ->where('user_id', '=', $request->user()->id)
+            ->where('incubator_id', '=', $incubator->id)
+            ->first();
+
+        if(!$ownership)
+            return response()->json(["Message" => 'Unauthorized']);
+        $data = Dioxide::query()->where('incubator_id', '=', $incubator->id)->get();
+        $count = Dioxide::query()->where('incubator_id', '=', $incubator->id)->count();
+
+        return response()->json([
+            "Count" => $count,
+            "Data"  => $data
+        ]);
+    }
+}
---
+++ database/migrations/2022_12_11_220132_create_dioxides_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dioxides', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedDecimal('value')->nullable(false);
+            $table->string('identifier')->nullable(false);
+            $table->foreignId('incubator_id')->constrained('incubators');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dioxides');
+    }
+};
